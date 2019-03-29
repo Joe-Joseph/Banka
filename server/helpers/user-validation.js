debc305bd08dcece21b604a75f57f8a24eaa3f97
Joseph <jostheblessing@gmail.com>
2019-03-29 18:19:12 +0200

    [Feature #165001355]Allow user to signup

---
+++ server/helpers/user-validation.js
@@ -0,0 +1,11 @@
+import joi from 'joi'
+
+exports.validateUser = (user)=>{
+    const userSchema = {
+        firstName : joi.string().min(3).required(),
+        lastName : joi.string().min(3).required(),
+        email : joi.string().email().required(),
+        password : joi.string().min(6).required()
+    }
+    return joi.validate(user, userSchema)
+}
