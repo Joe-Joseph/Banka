cd2bcc5b9d2b26b4f93a7cfc0a1228200c4db3cb
Joseph <jostheblessing@gmail.com>
2019-03-30 05:09:08 +0200

    [Feature #165003819]Allow logged in user to edit account number

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,13 +1,14 @@
 import express from 'express'
 import signupCtrl from '../controllers/signup'
 import loginCtrl from '../controllers/login'
-import createAccountCtrl from '../controllers/create-account'
+import accountCtrl from '../controllers/account'
 import auth from '../middleware/auth'
 
 const router  = express.Router()
 
 router.post('/auth/signup', signupCtrl.signup)
 router.post('/auth/signin', loginCtrl.login)
-router.post('/accounts', auth, createAccountCtrl.createAccount)
+router.post('/accounts', auth, accountCtrl.createAccount)
+router.patch('/accounts/:id', auth, accountCtrl.updateAccount)
 
 export default router
