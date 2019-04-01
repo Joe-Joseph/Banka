06ba1a07d95daf3664eba8002959abd0a0201a37
Joseph <jostheblessing@gmail.com>
2019-04-01 13:18:07 +0200

    [Feature #165019699] Allow staff  to debit account

--- server/routes/router.js
+++ server/routes/router.js
@@ -2,6 +2,7 @@ import express from 'express'
 import signupCtrl from '../controllers/signup'
 import loginCtrl from '../controllers/login'
 import accountCtrl from '../controllers/account'
+import transactionCtrl from '../controllers/transactions'
 import auth from '../middleware/auth'
 
 const router  = express.Router()
@@ -11,5 +12,6 @@ router.post('/auth/signin', loginCtrl.login)
 router.post('/accounts', auth, accountCtrl.createAccount)
 router.patch('/accounts/:accountNumber', auth, accountCtrl.updateAccount)
 router.delete('/accounts/:accountNumber', auth, accountCtrl.deleteAccount)
+router.post('/accounts/:accountNumber/debit', auth, transactionCtrl.debitAccount)
 
 export default router
