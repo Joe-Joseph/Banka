2842ed0cddc07488e58418b7de7b974724a236de
Joseph <jostheblessing@gmail.com>
2019-04-01 08:17:23 +0200

    [Feature #165015130] Allow user to delete bank account

--- server/routes/router.js
+++ server/routes/router.js
@@ -10,5 +10,6 @@ router.post('/auth/signup', signupCtrl.signup)
 router.post('/auth/signin', loginCtrl.login)
 router.post('/accounts', auth, accountCtrl.createAccount)
 router.patch('/accounts/:id', auth, accountCtrl.updateAccount)
+router.delete('/accounts/:id', auth, accountCtrl.deleteAccount)
 
 export default router
